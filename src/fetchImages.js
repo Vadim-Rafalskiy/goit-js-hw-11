758e21113406a44d4d1f5ea82badad8a08fa659e
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2022-12-23 05:16:38 +0200

    finish

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com';
 const KEY = '32223150-dddf312f26d3017ef1e9616a6';
 
-function params(searchQuery) {
+function params(searchQuery, nextPage) {
   return {
     params: {
       q: `${searchQuery}`,
@@ -11,13 +11,17 @@ function params(searchQuery) {
       orientation: 'horizontal',
       safesearch: 'true',
       per_page: '40',
+      page: `${nextPage}`, //*---------*
     },
   };
 }
 
-export async function fetchImages(searchQuery) {
+export async function fetchImages(searchQuery, nextPage) {
   try {
-    const response = await axios.get(`/api/?key=${KEY}`, params(searchQuery));
+    const response = await axios.get(
+      `/api/?key=${KEY}`,
+      params(searchQuery, nextPage)
+    );
     const data = await response.data;
 
     return data;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import './css/styles.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import SimpleLightbox from 'simplelightbox';
-import { failMessage, totalHitsMessage } from './messages';
+import { failMessage, totalHitsMessage, infoMessage } from './messages';
 import { fetchImages } from './fetchImages';
 import { getImageTemplate } from './getImageTemplate';
+
+const _ = require('lodash');
 
 const refs = {
   form: document.querySelector('.search-form'),
@@ -12,20 +15,31 @@ const refs = {
   gallery: document.querySelector('.gallery'),
 };
 
+let galleryLightbox = new SimpleLightbox('.gallery a');
+
 let images = [];
+let totalHits = 1;
+let nextPage = 1;
+let searchQuery = '';
+// let isNewQuery = true;
 
 refs.form.addEventListener('submit', onSearch);
 refs.gallery.addEventListener('click', handleClick);
+window.addEventListener('scroll', _.throttle(checkPosition, 500));
 
 function onSearch(e) {
+  window.scrollTo(top);
   e.preventDefault();
-  refs.button.setAttribute('disabled', true);
-  const searchQuery = e.target.searchQuery.value.trim();
 
-  if (searchQuery !== '')
-    fetchImages(searchQuery).then(date => {
-      images = date.hits;
-      date.total === 0 ? failMessage() : totalHitsMessage(date.total);
+  refs.button.setAttribute('disabled', true);
+
+  searchQuery = e.target.searchQuery.value.trim();
+
+  if (searchQuery)
+    fetchImages(searchQuery, (nextPage = 1)).then(data => {
+      images = data.hits;
+      totalHits = data.totalHits;
+      data.totalHits === 0 ? failMessage() : totalHitsMessage(data.totalHits);
 
       refs.gallery.innerHTML = '';
       render();
@@ -33,19 +47,40 @@ function onSearch(e) {
     });
 }
 
-const render = () => {
-  const cards = images.map(getImageTemplate);
-  refs.gallery.insertAdjacentHTML('beforeend', cards.join(''));
-  galleryLightbox.refresh();
-  refs.searchInput.value = '';
-};
-
-let galleryLightbox = new SimpleLightbox('.gallery a');
+function render() {
+  if (images !== []) {
+    refs.searchInput.value = '';
+    const cards = images.map(getImageTemplate);
+    refs.gallery.insertAdjacentHTML('beforeend', cards.join(''));
+    galleryLightbox.refresh();
+  }
+}
 
 function handleClick(e) {
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  }
   e.preventDefault();
+  if (e.target.nodeName !== 'IMG') return;
+
   galleryLightbox.on('shown.simpleLightbox');
+}
+
+function checkPosition() {
+  const height = document.body.offsetHeight;
+  const screenHeight = window.innerHeight;
+  const scrolled = window.scrollY;
+  const threshold = height - screenHeight;
+
+  const position = scrolled + screenHeight;
+  if (position >= height) {
+    infoMessage();
+  }
+  if (position >= threshold) {
+    //nextPage < totalHits && && isNewQuery
+    // isNewQuery = false;
+    nextPage += 1;
+    fetchImages(searchQuery, nextPage).then(data => {
+      images = data.hits;
+      render();
+      // isNewQuery = true;
+    });
+  }
 }
